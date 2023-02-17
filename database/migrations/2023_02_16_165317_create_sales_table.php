f05d857fb8fc8d6172771a928394101d08a84d1f
unknown <tab457893@gmail.com>
2023-02-17 16:32:04 -0500

    primera vercion

---
+++ database/migrations/2023_02_16_165317_create_sales_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id')->unsigned();/**usuario cliente */
+            $table->bigInteger('product_id')->unsigned();/**producto(s) a comprar*/
+            $table->date('date');
+            $table->timestamps();/**create_at updated_at*/
+            $table->softDeletes();/**delete_at*/
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sales');
+    }
+};
