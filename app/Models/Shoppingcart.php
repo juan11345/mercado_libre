524b399d187e326f3c533451789f58096e6861f3
unknown <tab457893@gmail.com>
2023-02-27 16:15:31 -0500

    3.2

---
+++ app/Models/Shoppingcart.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Models\Product;
+use App\Models\User;
+
+class Shoppingcart extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'customer_user_id',
+        'admin_user_id',
+        'product_id'
+    ];
+
+    public function Product(){
+        return $this -> belongsTo(Product::class, 'product', 'id');
+    }
+
+    public function Admin(){
+        return $this -> belongsTo(User::class, 'admin_user_product', 'id');
+    }
+
+    public function Customer(){
+        return $this -> belongsTo(Product::class, 'customer_user_product', 'id');
+    }
+}
